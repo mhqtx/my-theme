a9ec884fabe102132863ce77f52852c2b7c5382d
Milan Hakaj <milan.hakaj@quantox.com>
2024-01-20 15:07:23 +0100

    ff

--- app/_components/location-1.tsx
+++ app/_components/location-1.tsx
@@ -13,7 +13,7 @@ export function Location1() {
             We are here to help. Wherever you are, Vercel Client Advisors will
             be delighted to assist you
           </p>
-          <div className="mb-2">
+          <div className="mb-4">
             <h6 className="text-md mb-1 flex">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -39,35 +39,63 @@ export function Location1() {
             <h6 className="text-md mb-1 flex items-start">
               <div className="flex flex-col">
                 <span>Omladinskih brigada 72</span>
-                <span className="text-black/50">Belgrade, Serbia</span>
-                <span className="text-black/50">11000</span>
+                <span>Belgrade, Serbia</span>
+                <span>11000</span>
               </div>
             </h6>
           </div>
 
-          <h6 className="text-md mb-1 flex items-start">
-            <div className="flex flex-col">
-              <span>+38128328412</span>
-              <span className="text-black/50">Milan Hakaj</span>
-            </div>
+          <h6 className="text-md mb-1 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-3 w-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <span>Working hours</span>
           </h6>
-
-          <h6 className="text-md mb-2 flex items-start">
-            <div className="flex flex-col">
-              <span>milan.hakaj@mchnm.com</span>
-              <span className="text-black/50">Milan Hakaj</span>
-            </div>
-          </h6>
-
           {Object.entries(workingHours).map(([key, value]) => (
             <p key={key}>
               <span className="capitalize">{key}</span> {value}
             </p>
           ))}
 
+          <h6 className="text-md mt-4 flex space-x-0.5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-3 w-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+              />
+            </svg>
+            <span>Contact</span>
+          </h6>
+
+          <h6 className="text-md flex items-start">
+            <div className="flex flex-col">
+              <span>+38128328412 - Milan</span>
+              <span>+38124825443 - Sneźana</span>
+            </div>
+          </h6>
+
           <div className="mt-4 space-x-1">
             <Button>Get directions</Button>
-            <Button>Call now</Button>
+            <Button>Call us</Button>
           </div>
         </div>
       </div>
