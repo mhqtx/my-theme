98635a5b2299266ce2773bb6aaba141ed8eebe4e
Milan Hakaj <milan.hakaj@quantox.com>
2024-01-21 20:34:34 +0100

    qrrrr

--- app/_components/nav/dropdown.tsx
+++ app/_components/nav/dropdown.tsx
@@ -28,8 +28,8 @@ export default function Dropdown({ onClose }: Props) {
   }, []);
 
   return (
-    <div className="fixed right-0 top-6 z-40 flex h-full w-full flex-col items-end overflow-scroll bg-white p-2">
-      <div className="container">
+    <div className="fixed right-0 top-6 z-40 flex h-full w-full flex-col items-end overflow-scroll bg-white/[98] p-2">
+      <div className="container h-full">
         <div className="flex size-full items-center justify-center text-center text-black">
           <div>
             <h5 className="mb-2 font-bold">Explore</h5>
